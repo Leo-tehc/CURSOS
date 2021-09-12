a92c3de04a42232f891f5a6e5167eb3df2239e48
Leo-tehc <82243913+Leo-tehc@users.noreply.github.com>
2021-09-11 21:45:33 -0500

    Add files via upload

---
+++ PHP/PHP.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <tittle> Mi primera pagina PHP</tittle>
+    </head>
+    <body>
+        <? PHP echo "¡Hola mundo!"; ?> 
+        <? PHP echo "¡Me encanta PHP!";?>
+        <? PHP
+            echo "A";
+            echo "B";
+            echo "C";
+        ?>
+        <? PHP
+        echo "<strong> Este es un texto en negrita.
+        </strong>";
+        ?>
+        <? PHP
+        echo "<p> ¡Hola mundo! </p>";// Este es un 
+        echo de comentario de una sola línea "<p> ¡Estoy aprendiendo 
+        PHP! </p>"; echo "<p> ¡Este es mi primer programa! </p>"
+        <? php
+        $ nombre = 'Leonardo';
+        $ edad = 25;
+        echo $ nombre ; // ¿Muestra 'Jhon' ?>
+    </body>
+    <? php 
+    define ("MSG", "Hola, SoloLearners!");
+    echo MSG;  // Muestra "¡Hola, SoloLearners!"?>
+    <? php 
+    $ str = "10";
+    $ int = 20;
+    $ suma = $ str + $ int;
+    echo ($suma);  // ¿Salidas 30 ?>
+</html>
